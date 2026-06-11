f58d5b67a26065464f3a0a6f2519cfa22444ac9a
devdasgod1-sudo <devdasgod1@gmail.com>
2026-06-11 10:23:50 +0545

    fixing favicon vissible on serach bar

--- app/robots.ts
+++ app/robots.ts
@@ -4,10 +4,16 @@ const SITE_URL = (process.env.NEXT_PUBLIC_SITE_URL || "https://horizonnepalconst
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+      {
+        userAgent: "Googlebot-Image",
+        allow: ["/favicon.png", "/favicon.icon"], // Ensures Google's image crawler can always grab your icons
+      },
+    ],
     sitemap: `${SITE_URL}/sitemap.xml`,
   };
 }
